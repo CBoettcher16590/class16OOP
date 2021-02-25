e44fa0d38dcf500b37ab5bf6e1c17642ce8cad33
root <root@LAPTOP-EA6PCKJ1.localdomain>
2021-02-24 19:24:52 -0800

    created LoggedInUser singleton

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
-import { hello } from "./hello";
+import fs from 'fs';
 
-const message = hello();
-console.log(message);
+export default class LoggedInUser {
+
+    private static instance: LoggedInUser
+
+    constructor() {
+        if (LoggedInUser.instance = null) {
+            return new LoggedInUser();
+        }
+        return LoggedInUser.instance;
+    }
+
+    public connection() {
+        const path = `${__dirname}/users.json`;
+
+        fs.writeFile(path,'{}', function (err) {
+            if (err) {
+                return console.log(err);
+        }
+        })
+    }
+};
+
+const testing = new LoggedInUser();
+const user = testing.connection();
+
+console.log(user)
+
+
+
